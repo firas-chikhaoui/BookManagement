1d5dec4ffd73b9cafccb3b2d0b5891dc5f5e81ac
Firas Chikhaoui <firas.chikhaoui@esprit.tn>
2023-06-05 01:00:39 +0200

    added CRUD of books and the essentials settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ const Header: React.FC = () => {
     return (
         <header className='bg-blue-500 text-white p-4 mb-10'>
             <div className='container mx-auto flex items-center justify-between'>
-                <h1 className='text-2xl font-bold'>
-                    <nav>
+                <h1 className='text-2xl font-bold'>Book Management App</h1>
+                <nav>
                         <ul className='flex space-x-4'>
                             <li>
                                 <NavLink to='/' className = 'hover:underline'>
@@ -19,11 +19,11 @@ const Header: React.FC = () => {
                                 </NavLink>
                             </li>
                         </ul>
-                    </nav>
-                </h1>
+                </nav>
+
             </div>
         </header>
-
+        
     );
 };
 
